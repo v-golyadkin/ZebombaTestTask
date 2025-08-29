6fcd580749faaf316009183ef9c91a06eaa48aa2
Redream <mr.golyadkina@mail.ru>
2025-08-29 19:20:23 +0300

    Added method GetRandomColor

--- Assets/_Project/Logic/Scripts/CircleCreator.cs
+++ Assets/_Project/Logic/Scripts/CircleCreator.cs
@@ -1,14 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CircleCreator : Singleton<CircleCreator>
 {
     [SerializeField] private CircleView circleViewPrefab;
 
+    private List<Color> colorList = new List<Color>() { Color.green, Color.blue, Color.yellow };
+
     public CircleView CreateCircleView(Vector3 position, Quaternion rotate, Transform parent)
     {
-        Circle circle = new(Color.yellow);
+        Circle circle = new(GetRandomColor());
         CircleView circleView = Instantiate(circleViewPrefab, position, rotate, parent);
         circleView.Setup(circle);
         return circleView;
+    }
+
+    private Color GetRandomColor()
+    {
+        var randomColor = colorList[Random.Range(0, colorList.Count)];
+        return randomColor;
     }
 }
